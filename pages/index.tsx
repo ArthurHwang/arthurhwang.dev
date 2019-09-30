59ed6be395941d9a6a4b683bd92d62b0805aee75
ArthurHwang <ahwang55@gmail.com>
2019-09-29 22:02:19 -0700

    added styled components

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default function Home() {
+  return <StyledWrap>Wfasdfunky</StyledWrap>;
+}
+
+const StyledWrap = styled("div")`
+  color: red;
+  background: blue;
+`;
