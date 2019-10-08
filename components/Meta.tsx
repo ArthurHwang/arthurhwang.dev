03be873dc57edcc109732d96a957dd67947b33c9
ArthurHwang <ahwang55@gmail.com>
2019-10-08 10:47:58 -0700

    add favicon files

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -4,7 +4,24 @@ export default () => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
-    <link rel="shortcut icon" href="/static/favicon.png" />
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="static/favicon/apple-touch-icon.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="static/favicon/favicon-32x32.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="static/favicon/favicon-16x16.png"
+    />
+    <link rel="manifest" href="/favicon/site.webmanifest" />
     <title>ArthurHwang.dev</title>
   </Head>
 );
