15b65061454f68b1dc77bdaef2bed29397e6532e
ArthurHwang <ahwang55@gmail.com>
2019-10-11 11:58:43 -0700

    define a few projects #2

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -9,29 +9,20 @@ export default function Projects() {
         <meta name="description" content="Arthur Hwang's Blog" />
       </Head>
       <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea magnam cum
-        a aut quasi nulla! Commodi, expedita! Doloremque sed cum placeat
-        voluptatibus unde tempora, facere sapiente illum consequuntur assumenda
-        sunt quae facilis aperiam sequi, accusantium quo officiis temporibus
-        odio ut maxime! Eveniet labore excepturi accusantium molestiae iure
-        maiores odit dolores id, nemo aliquam repellendus et laborum adipisci
-        ullam voluptatum sint voluptate ut consequatur quibusdam explicabo quasi
-        reprehenderit veritatis temporibus quas. Similique temporibus porro
-        suscipit dolorem ullam veniam voluptatum magnam, debitis rerum
-        doloribus. Ipsam dolorem dolores sunt quia itaque recusandae
-        reprehenderit id est explicabo suscipit deserunt, corrupti aut numquam
-        aspernatur, laborum a non quidem maxime! Modi incidunt sint reiciendis
-        ex quisquam alias, maxime deserunt vero architecto sapiente quia nihil
-        soluta obcaecati itaque aliquam libero. Repellat similique velit
-        provident nulla illo consectetur eos ullam magnam repudiandae tenetur
-        excepturi quisquam dolores, necessitatibus, ratione vero? Qui, quaerat
-        esse perferendis harum vel excepturi vero totam doloremque laboriosam
-        possimus magnam tempore obcaecati quia quas dolorem fuga cupiditate
-        soluta dicta, iste et eligendi suscipit! Minus beatae unde excepturi non
-        maiores commodi fugit officia nihil, obcaecati sequi quibusdam dolore,
-        illum, alias rem deserunt cumque tempore! Obcaecati inventore excepturi
-        corporis fugiat dolore voluptatum nisi autem, nostrum distinctio, veniam
-        qui.
+        <h3>bestattorney.netlify.com</h3>
+        gatsby cypress react hooks context API, useReducerAPI, jest graphql
+        custom hosting headless CMS wordpress . Smoke tests, unit tests, E2E
+        tests
+        <h3>arthurhwang.dev</h3>
+        apollo graphql nextjs contentful cloudflare ziet now
+        <h3>
+          bestattorney.com angular user login / user authentication system
+        </h3>
+        cookies local storage angular reverse proxy
+        <h4>toneify</h4>
+        react redux mongodb express
+        <h4>DS ALGOS</h4>
+        DS-ALGO
       </div>
     </Fragment>
   );
