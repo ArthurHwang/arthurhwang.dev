d406db985b60d7a154130159bbb81bb3737be57b
ArthurHwang <ahwang55@gmail.com>
2019-10-19 11:34:24 -0700

    improve error handling #16

--- pages/api/contact.tsx
+++ pages/api/contact.tsx
@@ -12,30 +12,27 @@ const auth = {
 const nodemailerMailgun = nodemailer.createTransport(mg(auth));
 
 export default function handle(req: any, res: any) {
-  try {
-    nodemailerMailgun.sendMail(
-      {
-        from: "mail@arthurhwang.dev",
-        to: "ahwang55@gmail.com", // An array if you have multiple recipients.
-        subject: "New contact form submission from arthurhwang.dev",
-        html: `<b>From: ${req.body.firstName} ${req.body.lastName}</b>
+  nodemailerMailgun.sendMail(
+    {
+      from: "mail@arthurhwang.dev",
+      to: "ahwang55@gmail.com", // An array if you have multiple recipients.
+      subject: "New contact form submission from arthurhwang.dev",
+      html: `<b>From: ${req.body.firstName} ${req.body.lastName}</b>
                <br />    
                <b>Email: ${req.body.email}</b>
                <br />
                <br />
                <b>Message: ${req.body.message}</b>`
-      },
-      function(err: any, info: any) {
-        if (err) {
-          console.log("Error: " + err);
-        } else {
-          console.log("Response: " + info);
-        }
+    },
+    function(err: any, info: any) {
+      if (err) {
+        res.status(500).json({ message: "Failed to send message, try again." });
+        console.log("Error: " + err);
+      } else {
+        res.status(200).json({ message: "Message sent, thank you." });
+        console.log("Response: " + info);
       }
-    );
-    res.status(200).json({ message: "Message sent, thank you." });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Failed to send message, try again." });
-  }
+    }
+  );
+  //   res.status(200).json({ message: "Message sent, thank you." });
 }
