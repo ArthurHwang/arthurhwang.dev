d4daf6dcf73b3f329d0c853103c3099c3a40462c
ArthurHwang <ahwang55@gmail.com>
2019-10-19 11:31:33 -0700

    update webpack env vars #16

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
   env: {
     SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+    MAILGUN_APIKEY: process.env.MAILGUN_APIKEY,
     REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN:
       process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
   }
