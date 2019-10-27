b9c81e1b7308a3b9704275bfb6ba290b6276730b
ArthurHwang <ahwang55@gmail.com>
2019-10-27 02:50:29 -0700

    test fade true carousel prop

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -23,7 +23,7 @@ const Projects: NextPage<any> = () => {
     autoplaySpeed: 4000,
     pauseOnHover: false,
     speed: 700,
-    // fade: true,
+    fade: true,
     // lazyLoad: "progressive",
     adaptiveHeight: true
   };
