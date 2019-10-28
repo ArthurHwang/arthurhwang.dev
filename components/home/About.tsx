39e6bec40f5a4468d266e5c70693042eb5d3d5d6
ArthurHwang <ahwang55@gmail.com>
2019-10-27 22:29:04 -0700

    test text change #29

--- components/home/About.tsx
+++ components/home/About.tsx
@@ -8,25 +8,16 @@ export const About: React.FC<any> = () => {
           About me<span>_</span>
         </h2>
         <p>
-          I’ve always sought out opportunities and challenges that are
-          meaningful to me. Although my professional path has taken many twists
-          and turns — from touring and recording artist, to employee of the year
-          at a non-profit, to dean's scholar at UPenn, to small business owner
-          and entrepreneur — I've never stopped engaging my passion to help
-          others and solve problems. As a web developer, I enjoy using my
-          obsessive attention to detail, my unequivocal love for making things,
-          and my mission-driven work ethic to literally change the world. That's
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+          nam ducimus sit maiores a quo ea natus fugiat cupiditate porro neque
+          iste, temporibus iusto nobis expedita iure officia reiciendis optio.
           why I’m excited to make a big impact at a high growth company.
         </p>
         <p>
-          My name is Kyle. I design and build digital products that are
-          intuitive, accessible, beautiful, and fun. I’ve done this
-          professionally since 2012, and I’ve been designing for the web since
-          2007. This is a home for things I’ve made and how I’ve made them. My
-          goal is for anyone (and anyone’s grandma) to understand what I’ve
-          written. Although the focus is on my design and the process behind it,
-          case studies occasionally detour to explore interesting, relevant
-          topics.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+          corrupti laborum impedit iste consequuntur eius odit odio adipisci
+          omnis, cupiditate maiores culpa suscipit perferendis explicabo beatae
+          eveniet! Asperiores, est sapiente.
         </p>
       </StyledAbout>
     </ContentWrap>
